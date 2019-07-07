752f67000c7c0919f1e1ca9628136f2e00366fb4
Wesley Kerfoot <wjak56@gmail.com>
2019-07-07 03:51:58 -0400

    add --pedantic

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
-$CC --std=gnu11 $(pkg-config --cflags --libs cairo x11 x11-xcb xcb gl glu xcb-glx) $1
+$CC -Wall --pedantic --std=gnu11 $(pkg-config --cflags --libs cairo x11 x11-xcb xcb gl glu xcb-glx) $1
 
